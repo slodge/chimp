bb4fdd5f2ed2f82a97116ac5d169eeaf722f935c
Stuart <lodge.stuart@gmail.com>
2012-01-22 09:41:59 +0000

    I suppose it might help if it builds - sorry!

--- NinetyNinePercent/MainPage.xaml.cs
+++ NinetyNinePercent/MainPage.xaml.cs
@@ -41,39 +41,6 @@ namespace NinetyNinePercent
             SelectedImage.Source = image;
         }
 
-        private void DisplayResult(BitmapImage bitmapImage, FaceRestAPI.FaceAPI result)
-        {
-            DisplayChimpResult(bitmapImage, result);
-        }
-
-        private void DisplayChimpResult(BitmapImage bitmapImage, FaceRestAPI.FaceAPI result)
-        {
-            var writeable = new WriteableBitmap(bitmapImage);
-
-            foreach (var tag in result.photos[0].tags)
-            {
-                // Uses the segment's center and half width, height
-                var c = tag.center;
-                Debug.WriteLine("Center ({0} ,{1}), size ({2}, {3})", c.x, c.y, tag.width,
-                                tag.height);
-                var destRect = new Rect(bitmapImage.PixelWidth * 0.01 * (tag.center.x - tag.width / 2),
-                                        bitmapImage.PixelHeight * 0.01 * (tag.center.y - tag.height / 2),
-                                        bitmapImage.PixelWidth * 0.01 * tag.width,
-                                        bitmapImage.PixelHeight * 0.01 * tag.height);
-                var sourceRect = new Rect(0, 0, _chimpWriteableBitmap.PixelWidth, _chimpWriteableBitmap.PixelHeight);
-                writeable.Blit(destRect, _chimpWriteableBitmap, sourceRect);
-            }
-
-            writeable.Invalidate();
-            SelectedImage.Source = writeable;
-
-            writeable.SaveToFile("SaveMe.jpg", (stream) =>
-            {
-                var library = new MediaLibrary();
-                Picture pic = library.SavePicture("SavedPicture.jpg", stream);
-            });
-        }
-
         private void ApplicationBarIconButton_Camera_Click(object sender, EventArgs e)
         {
             var task = new CameraCaptureTask();
@@ -233,63 +200,3 @@ namespace NinetyNinePercent
     }
 }
 
-
-
-
-/*
-private void DisplayJediResult(BitmapImage bitmapImage, FaceRestAPI.FaceAPI result)
-{
-    var writeable = _jediWriteableBitmap;
-
-    foreach (var tag in result.photos[0].tags)
-    {
-        // Uses the segment's center and half width, height
-        var c = tag.center;
-        Debug.WriteLine("Center ({0} ,{1}), size ({2}, {3})", c.x, c.y, tag.width,
-                        tag.height);
-        var sourceRect = new Rect(bitmapImage.PixelWidth * 0.01 * (tag.center.x - tag.width / 2),
-                                bitmapImage.PixelHeight * 0.01 * (tag.center.y - tag.height / 2),
-                                bitmapImage.PixelWidth * 0.01 * tag.width,
-                                bitmapImage.PixelHeight * 0.01 * tag.height);
-        var destRect = new Rect(288, 103, 63, 86);
-        writeable.Blit(destRect, new WriteableBitmap(bitmapImage), sourceRect);
-    }
-
-    writeable.Invalidate();
-    JediImage.Source = writeable;
-
-    writeable.SaveToFile("SaveMe.jpg", (stream) =>
-                                            {
-                                                MediaLibrary library = new MediaLibrary();
-                                                Picture pic = library.SavePicture("SavedPicture.jpg", stream);
-                                            });
-}
-*/
-
-
-/*
-private void LoadJedi()
-{
-    var image = new BitmapImage(new Uri("/Images/Jedi1.jpg", UriKind.Relative));
-    image.ImageOpened += (sender, args) => { _jediWriteableBitmap = new WriteableBitmap(image); };
-    JediImage.Source = image;
-}
-*/
-
-/*
-private void ProcessImage2(BitmapImage bitmapImage)
-{
-    string tempFileName = "temp.jpg";
-    bitmapImage.SaveToFile(tempFileName);
-    string apikey = "96fbe5ef96fb2ea460999459b92bb951";
-    string secretkey = "d6505a7ebe7c7f51e72d4e75c46082f8";
-    var api = new FaceRestAPI(apikey, secretkey, "", false, "json", "", "");
-    api.faces_detect(null, tempFileName, null, string.Empty,
-                     (result) =>
-                         {
-                             Debug.WriteLine("Hello " + result);
-                             Dispatcher.BeginInvoke(() =>DisplayResult(bitmapImage, result));
-                         },
-                         (error) => Debug.WriteLine("Bummer " + error));
-}
- */
